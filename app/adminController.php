816649c22e6d0f77cf8366284ba4701d011adeba
easyScript <prezentabilan@gmail.com>
2013-10-06 00:19:10 +0200

    add administrator views loading

--- app/adminController.php
+++ app/adminController.php
@@ -3,10 +3,20 @@
 $admin = $app['controllers_factory'];
 $admin->get('/dashboard', function (Silex\Application $app)
   {
+    $pageId = 'dashboard';
     return $app['twig']->render('admin/dashboard.twig', array(
-          'pageId' => 'admin',
-          'title'  => 'Admin'
+          'pageId' => $pageId,
+          'title'  => ucfirst($pageId)
     ));
   })->bind('dashboard');
+
+$admin->get('/articles', function (Silex\Application $app)
+  {
+    $pageId = 'articles';
+    return $app['twig']->render('admin/articles.twig', array(
+          'pageId' => $pageId,
+          'title'  => ucfirst($pageId)
+    ));
+  })->bind('articles');
 
 return $admin;
