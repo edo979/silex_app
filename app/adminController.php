ab6e59ccaf9ff7d2a5b43be22f4fd98caa2a2f39
easyScript <prezentabilan@gmail.com>
2013-10-06 01:35:08 +0200

    add basic admin controller and query for all article

--- app/adminController.php
+++ app/adminController.php
@@ -13,10 +13,25 @@ $admin->get('/dashboard', function (Silex\Application $app)
 $admin->get('/articles', function (Silex\Application $app)
   {
     $pageId = 'articles';
+    $conn = $app['db'];
+
+    $statement = $conn->executeQuery('SELECT * FROM articles');
+    $articles = $statement->fetchAll();
+
     return $app['twig']->render('admin/articles.twig', array(
+          'pageId' => $pageId,
+          'title'  => ucfirst($pageId),
+          'articles'=> $articles
+    ));
+  })->bind('articles');
+  
+  $admin->get('/article/{id}', function (Silex\Application $app, $id)
+  {
+    $pageId = 'article';
+    return $app['twig']->render('admin/dashboard.twig', array(
           'pageId' => $pageId,
           'title'  => ucfirst($pageId)
     ));
-  })->bind('articles');
+  })->bind('article');
 
 return $admin;
