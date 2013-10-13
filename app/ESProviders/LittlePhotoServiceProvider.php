a61a47223e18028da3e56da02b676214100c1fe7
easyScript <prezentabilan@gmail.com>
2013-10-14 01:29:33 +0200

    finishing atach_file method

--- app/ESProviders/LittlePhotoServiceProvider.php
+++ app/ESProviders/LittlePhotoServiceProvider.php
@@ -7,6 +7,24 @@ use Silex\ServiceProviderInterface;
 
 class LittlePhotoServiceProvider implements ServiceProviderInterface
 {
+
+  private $temp_path;
+  private $filename;
+  private $type;
+  private $size;
+  private $errors = array();
+  private $upload_errors = array(
+      // http://www.php.net/manual/en/features.file-upload.errors.php
+      UPLOAD_ERR_OK         => "No errors.",
+      UPLOAD_ERR_INI_SIZE   => "Larger than upload_max_filesize.",
+      UPLOAD_ERR_FORM_SIZE  => "Larger than form MAX_FILE_SIZE.",
+      UPLOAD_ERR_PARTIAL    => "Partial upload.",
+      UPLOAD_ERR_NO_FILE    => "No file.",
+      UPLOAD_ERR_NO_TMP_DIR => "No temporary directory.",
+      UPLOAD_ERR_CANT_WRITE => "Can't write to disk.",
+      UPLOAD_ERR_EXTENSION  => "File upload stopped by extension."
+  );
+
   public function boot(Application $app)
   {
     
@@ -14,12 +32,40 @@ class LittlePhotoServiceProvider implements ServiceProviderInterface
 
   public function register(Application $app)
   {
-    $app['photoHandler'] = $app->protect(function() use($app) {
-      
+    $app['photoHandler'] = $app->protect(function() use($app)
+      {
+        
       });
-  }  
-}
+  }
 
-// Get file from form
+  // Get file from form
+  private function attach_file($file)
+  {
+    // Perform error checking on the form parameters
+    if (!$file || empty($file) || !is_array($file))
+    {
+      // error: nothing uploaded or wrong argument usage
+      $this->errors[] = "No file was uploaded.";
+      return false;
+    }
+    elseif ($file['error'] != 0)
+    {
+      // error: report what PHP says went wrong
+      $this->errors[] = $this->upload_errors[$file['error']];
+      return false;
+    }
+    else
+    {
+      // Set object attributes to the form parameters.
+      $this->temp_path = $file['tmp_name'];
+      $this->filename = basename($file['name']);
+      $this->type = $file['type'];
+      $this->size = $file['size'];
+
+      return true;
+    }
+  }
+
 // Validate file
 // Save file to db
+}
--- tests/app/ESProviders/LittlePhotoServiceProviderTest.php
+++ tests/app/ESProviders/LittlePhotoServiceProviderTest.php
@@ -4,17 +4,66 @@ class LittlePhotoServiceProviderTest extends PHPUnit_Framework_TestCase
 {
 
   public $app;
+  protected $reflector;
+  protected $testClass;
 
   public function setUp()
   {
     $this->app = new Silex\Application();
     $this->app->register(new ESProviders\LittlePhotoServiceProvider());
+
+    $this->testClass = new ESProviders\LittlePhotoServiceProvider;
+    $this->reflector = new ReflectionClass($this->testClass);
   }
 
   public function testRegisteringProvider()
   {
     // Simple check is provider register and have set namespace
     assertEquals($this->app['photoHandler'], $this->app['photoHandler']);
+  }
+
+  public function testAttachFileFromFormToHandler()
+  {
+    $attache_file = $this->reflector->getMethod('attach_file');
+    $attache_file->setAccessible(true);
+    $attache_file->invoke($this->testClass, array(
+        'error'    => 0,
+        'tmp_name' => 'temp',
+        'name'     => '/images/image.jpg',
+        'type'     => 'jpg',
+        'size'     => 600
+    ));
+
+    $handlerProp = $this->reflector->getProperty('temp_path');
+    $handlerProp->setAccessible(true);
+    assertEquals('temp', $handlerProp->getValue($this->testClass));
+
+    $handlerProp = $this->reflector->getProperty('filename');
+    $handlerProp->setAccessible(true);
+    assertEquals('image.jpg', $handlerProp->getValue($this->testClass));
+    
+    $handlerProp = $this->reflector->getProperty('type');
+    $handlerProp->setAccessible(true);
+    assertEquals('jpg', $handlerProp->getValue($this->testClass));
+    
+    $handlerProp = $this->reflector->getProperty('size');
+    $handlerProp->setAccessible(true);
+    assertEquals(600, $handlerProp->getValue($this->testClass));
+    
+    // check for errors uploading image
+    $attache_file->invoke($this->testClass, 'formError');
+    
+    $handlerProp = $this->reflector->getProperty('errors');
+    $handlerProp->setAccessible(true);
+    assertEquals(array('No file was uploaded.'), $handlerProp->getValue($this->testClass));
+    
+    $attache_file->invoke($this->testClass, array(
+        'error'    => 4
+    ));
+    
+    $handlerProp = $this->reflector->getProperty('errors');
+    $handlerProp->setAccessible(true);
+    assertEquals('No file.', $handlerProp->getValue($this->testClass)[1]);
   }
 
 }
