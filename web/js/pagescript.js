26048373da6a8a30afa3c4a4b18c16da4784384a
easyScript <prezentabilan@gmail.com>
2013-10-23 00:31:52 +0200

    fix navbar using jquery

--- web/js/pagescript.js
+++ web/js/pagescript.js
@@ -1,10 +1,17 @@
-$(function() {
 // Active tab in navbar
-// TODO write loop for adding class to meny
-  $("#home nav.navbar li a:contains('Home')").parent().addClass('active');
-  $("#gallery nav.navbar li a:contains('Gallery')").parent().addClass('active');
-  $("#about nav.navbar ul li a:contains('About')").first().parent().addClass('active');
-  $("#example nav.navbar ul li a:contains('Example')").first().parent().addClass('active');
-  $("#contact nav.navbar ul li a:contains('Contact')").first().parent().addClass('active');
-  $("#login nav.navbar ul li a:contains('Admin')").first().parent().addClass('active');
+$(function() {
+  var url = document.location.pathname,
+      page = '', pageUp = '';
+      
+  if(url.length == 1){
+    page = 'home'; pageUp = 'Home';
+  }else {
+    page = url.substr(url.lastIndexOf('/') + 1);
+    pageUp = page.charAt(0).toUpperCase() + page.slice(1);
+  }
+  
+  $("#" + page + " nav.navbar li a:contains('"+pageUp+"')")
+    .first()
+    .parent()
+    .addClass('active');
 });
